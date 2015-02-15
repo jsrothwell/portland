e03c9b2c88456cb3788027720aebf79fb189b539
jsrothwell <jamiesonrothwell@gmail.com>
2015-02-14 22:57:19 -0800

    added classes to template to use materialize' grid

--- content-page.php
+++ content-page.php
@@ -11,7 +11,7 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content row">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
      <fieldset id="fieldset" class="fieldset">
          <label for="s" class="screen-reader-text">Search for:</label>
-         <input type="search" id="s" name="s" placeholder="Enter keywords" required />
-         <sub>Type in your query and press return</sub>
+         <input type="search" id="s" name="s" placeholder="Enter your query here" required />
+         <p>Type in your query and press return</p>
      </fieldset>
 </form>
