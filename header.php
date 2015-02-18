a3273f0118fed0ca87d114b2e45acc3e2ac6cdec
jsrothwell <jamiesonrothwell@gmail.com>
2015-02-17 19:58:49 -0800

    changes

--- header.php
+++ header.php
@@ -21,17 +21,17 @@
 <div id="page" class="hfeed site col s12">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'portland' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+	<header id="masthead" class="site-header row" role="banner">
+		<div class="site-branding col s6">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<!--h2 class="site-description">< ?php bloginfo( 'description' ); ?></h2-->
             
 		</div><!-- .site-branding -->
-
+            <div class="col s6 right">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( '<i class="mdi-navigation-menu"></i>', 'portland' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'header-menu-dropdown', 'container_class' => 'header-menu-container' ) ); ?>
-		</nav><!-- #site-navigation -->
+		</nav><!-- #site-navigation --></div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content container col s12">
