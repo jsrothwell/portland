a13819d9d8e7cae00917a14cae48edcb87c77ddf
jsrothwell <jamiesonrothwell@gmail.com>
2015-02-18 16:26:07 -0800

    revision

--- functions.php
+++ functions.php
@@ -168,3 +168,16 @@ function the_title_trim($title) {
 	return $title;
 }
 add_filter('the_title', 'the_title_trim');
+
+add_action( 'init', 'cd_add_editor_styles' );
+/**
+ * Apply theme's stylesheet to the visual editor.
+ *
+ * @uses add_editor_style() Links a stylesheet to visual editor
+ * @uses get_stylesheet_uri() Returns URI of theme stylesheet
+ */
+function cd_add_editor_styles() {
+
+    add_editor_style( get_stylesheet_uri() );
+
+}
