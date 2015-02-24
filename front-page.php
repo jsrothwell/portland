5ab7130ec9019c37f8484aae605a4f2613692b57
jsrothwell <jamiesonrothwell@gmail.com>
2015-02-23 17:02:01 -0800

    Updated blog template from home to front-page

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page-home' ); ?>
+				<?php get_template_part( 'content', 'page-front' ); ?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
